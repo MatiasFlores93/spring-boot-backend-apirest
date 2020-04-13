e522a907b9160e3e7faed35fd7062559a539ff12
Matias Eduardo Flores Farias <31407903+Mathias182@users.noreply.github.com>
2020-04-13 19:05:40 -0400

    Initial commit

---
+++ src/main/java/com/matias/springboot/backend/apirest/models/services/IClienteService.java
@@ -0,0 +1,11 @@
+package com.matias.springboot.backend.apirest.models.services;
+
+import java.util.List;
+
+import com.matias.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteService {
+	
+	public List<Cliente> findAll(); 
+
+}
